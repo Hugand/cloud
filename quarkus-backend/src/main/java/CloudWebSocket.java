892329e788ce5759d40345832cda01255ad6c273
Hugo Gomes <hugoasgomes@hotmail.com>
2020-10-26 00:48:11 +0000

    Mock of file list broadcast with websockets

--- quarkus-backend/src/main/java/CloudWebSocket.java
+++ quarkus-backend/src/main/java/CloudWebSocket.java
@@ -1,8 +1,8 @@
-package org.acme.websockets;
+package org.ugomes.websockets;
 
 import java.util.Map;
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -12,42 +12,76 @@ import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import javax.websocket.Session;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.ugomes.controllers.CloudDirectoryController;
+
+import com.google.gson.Gson; 
+
 @ServerEndpoint("/chat/{username}")         
 @ApplicationScoped
 public class CloudWebSocket {
-    Map<String, Session> sessions = new ConcurrentHashMap<>(); 
+    // Map<String, Session> sessions = new ConcurrentHashMap<>(); 
+    private ArrayList<Session> sessions = new ArrayList<>();
+    private Timer timer = new Timer();
+
+    public CloudWebSocket() {
+        timer.schedule(new CloudFilesTimer(this), 0, 1000);
+    }
 
     @OnOpen
     public void onOpen(Session session, @PathParam("username") String username) {
-        sessions.put(username, session);
+        // sessions.put(username, session);
+        sessions.add(session);
         broadcast("User " + username + " joined");
     }
 
     @OnClose
     public void onClose(Session session, @PathParam("username") String username) {
-        sessions.remove(username);
+        sessions.remove(session);
         broadcast("User " + username + " left");
     }
 
     @OnError
     public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
-        sessions.remove(username);
+        sessions.remove(session);
         broadcast("User " + username + " left on error: " + throwable);
     }
 
     @OnMessage
     public void onMessage(String message, @PathParam("username") String username) {
-        broadcast(">> " + username + ": " + message);
+        // broadcast(">> " + username + ": " + message);
     }
 
-    private void broadcast(String message) {
-        sessions.values().forEach(s -> {
-            s.getAsyncRemote().sendObject(message, result ->  {
+    public void broadcast(String message) {
+        sessions.forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result -> {
                 if (result.getException() != null) {
                     System.out.println("Unable to send message: " + result.getException());
                 }
             });
         });
     }
+}
 
+class CloudFilesTimer extends TimerTask {
+    private String[] lastFileList;
+    private CloudDirectoryController cloudDirectoryController = new CloudDirectoryController();
+    CloudWebSocket cloudWebSocket;
+
+    public CloudFilesTimer(CloudWebSocket cloudWebSocket) {
+        this.cloudWebSocket = cloudWebSocket;
+    }
+
+    public void run() {
+        Gson gson = new Gson();
+        String[] filesList = cloudDirectoryController.getFilesList();
+        String jsonStringified = gson.toJson(filesList);
+
+        if(filesList != this.lastFileList) {
+            this.cloudWebSocket.broadcast(jsonStringified);
+            this.lastFileList = filesList;
+        }
+    }
 }
--- quarkus-backend/src/main/java/controllers/CloudDirectoryController.java
+++ quarkus-backend/src/main/java/controllers/CloudDirectoryController.java
@@ -1,4 +1,35 @@
-package com.ugomes.controllers;
+package org.ugomes.controllers;
+
+import java.util.Map;
+import java.util.ArrayList;
+import java.io.File;
+import org.ugomes.configs.CloudProperties;
+// import org.eclipse.microprofile.config.inject.ConfigProperty;
+import javax.inject.Inject;
 
 public class CloudDirectoryController {
+    public CloudDirectoryController() { 
+    }
+
+    @Inject
+    CloudProperties cloud = new CloudProperties();
+
+    public String[] getFilesList() {
+        File cloudDirFiles = new File(CloudProperties.dir);
+
+        String[] pathnames = cloudDirFiles.list();
+
+        // for (String pathname : pathnames) {
+        //     // Print the names of files and directories
+        //     System.out.println(pathname);
+        // }
+
+        // HashMap<String, String> map = new HashMap<>();
+        // map.put("key", "value");
+        // map.put("foo", "bar");
+        // map.put("aa", CLOUD_DIR);
+        
+
+        return pathnames;
+    }
 }
