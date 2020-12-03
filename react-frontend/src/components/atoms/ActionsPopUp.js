4e07d747c062681c5368770d72adfb876c70cff8
Hugo Gomes <hugoasgomes@hotmail.com>
2020-12-03 02:19:34 +0000

    Basic popup implementation

---
+++ react-frontend/src/components/atoms/ActionsPopUp.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../../styles/atoms/actions-popup.scss'
+
+
+function ActionsPopUp() {
+
+
+    return (
+        <div className="actions-popup-container">
+            <button>Rename</button>
+            <button>Move</button>
+            <button>Delete</button>
+        </div>
+    )
+}
+
+export default ActionsPopUp
