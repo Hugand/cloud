50ad93d0d07527b830259a5d1df13b70df0ab9c7
Hugo Gomes <hugoasgomes@hotmail.com>
2020-12-11 01:13:35 +0000

    Add file icons

--- react-frontend/src/components/blocks/FileTypeStorageList.js
+++ react-frontend/src/components/blocks/FileTypeStorageList.js
@@ -9,32 +9,32 @@ function FileTypeStorageList({ storageData }) {
     return (
         <div className="file-type-desc">
             <div className="file">
-                <div className="file-type-icon"></div>
+                <img className="file-type-icon" src="./assets/icons/file_icons/image_file.svg" alt="file icon" />
                 <label className="file-type dark-text">Image</label>
                 <label className="space-used light-text">{ getFileSize(storageData.storageInImages) }</label>
             </div>
             <div className="file">
-                <div className="file-type-icon"></div>
+                <img className="file-type-icon" src="./assets/icons/file_icons/video_file.svg" alt="file icon" />
                 <label className="file-type dark-text">Videos</label>
                 <label className="space-used light-text">{ getFileSize(storageData.storageInVideos) }</label>
             </div>
             <div className="file">
-                <div className="file-type-icon"></div>
+                <img className="file-type-icon" src="./assets/icons/file_icons/audio_file.svg" alt="file icon" />
                 <label className="file-type dark-text">Audio</label>
                 <label className="space-used light-text">{ getFileSize(storageData.storageInAudio) }</label>
             </div>
             <div className="file">
-                <div className="file-type-icon"></div>
+                <img className="file-type-icon" src="./assets/icons/file_icons/docs_file.svg" alt="file icon" />
                 <label className="file-type dark-text">Docs</label>
                 <label className="space-used light-text">{ getFileSize(storageData.storageInDocs) }</label>
             </div>
             <div className="file">
-                <div className="file-type-icon"></div>
+                <img className="file-type-icon" src="./assets/icons/file_icons/other_file.svg" alt="file icon" />
                 <label className="file-type dark-text">Others</label>
                 <label className="space-used light-text">{ getFileSize(storageData.storageInOthers) }</label>
             </div>
             <div className="file">
-                <div className="file-type-icon"></div>
+                <img className="file-type-icon" src="./assets/icons/file_icons/free_space_file.svg" alt="file icon" />
                 <label className="file-type dark-text">Available</label>
                 <label className="space-used light-text">{ getFileSize(storageData.currentAvailableSpace) }</label>
             </div>
